021fbfdc4c41869f1f3172aab24fe622cac382cf
MfonOtung <mfonotung@gmail.com>
2024-01-02 15:59:55 +0000

    Solutions To More Challenges of Expressions and Statements

---
+++ src/main/java/org/mfon/section5/MethodOverloading.java
@@ -0,0 +1,39 @@
+package org.mfon.section5;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+    calculateScores("Berty", 80);
+    calculateScores(98);
+    int newScore = calculateScores("Sam", 78);
+        System.out.println("New sore is " + newScore);
+        System.out.println(calculateScores("Rocky"));
+        System.out.println(convertToCentimeters(68));
+        System.out.println(convertToCentimeters(5, 8));
+    }
+
+    public static int calculateScores(String name, int score) {
+        System.out.println(name + " scored " + score);
+        return score * 100;
+
+    }
+
+    public static void calculateScores( int score) {
+        System.out.println( "The score is " + score);
+    }
+
+    public static int calculateScores(String name) {
+        return calculateScores("Rocky", 65);
+    }
+
+    public static double convertToCentimeters(int heightInches) {
+        double centimeters = heightInches * 2.54;
+        return centimeters;
+    }
+
+    public static double convertToCentimeters(int heightFeet, int heightInches) {
+        int convertedHeightInches = heightFeet * 12;
+        int finalValue = convertedHeightInches + heightInches;
+        return convertToCentimeters(finalValue);
+    }
+
+}
