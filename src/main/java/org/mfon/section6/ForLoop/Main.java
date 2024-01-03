5a5d7301f445a881ae08962e303c12f8350adf20
MfonOtung <mfonotung@gmail.com>
2024-01-03 15:46:59 +0000

    Prime Number Coding

--- src/main/java/org/mfon/section6/ForLoop/Main.java
+++ src/main/java/org/mfon/section6/ForLoop/Main.java
@@ -14,11 +14,36 @@ public class Main {
 
         for (double rateInDollar = 7.5; rateInDollar <= 10; rateInDollar += 0.25){
             double interestAmountInDollar = calculateInterest(100.0, rateInDollar);
+            if (interestAmountInDollar > 8.5){
+                break;}
             System.out.println("100$ at " + rateInDollar + " % interest = " + interestAmountInDollar);
         }
+
+        System.out.println("0 is " + (isPrime(0) ? "" : "NOT ") + " a prime number");
+        System.out.println("1 is " + (isPrime(1) ? "" : "NOT ") + " a prime number");
+        System.out.println("2 is " + (isPrime(2) ? "" : "NOT ") + " a prime number");
+        System.out.println("3 is " + (isPrime(3) ? "" : "NOT ") + " a prime number");
+        System.out.println("4 is " + (isPrime(4) ? "" : "NOT ") + " a prime number");
+        System.out.println("5 is " + (isPrime(5) ? "" : "NOT ") + " a prime number");
+        System.out.println("6 is " + (isPrime(6) ? "" : "NOT ") + " a prime number");
+        System.out.println("7 is " + (isPrime(7) ? "" : "NOT ") + " a prime number");
+        System.out.println("8 is " + (isPrime(8) ? "" : "NOT ") + " a prime number");
+        System.out.println("17 is " + (isPrime(17) ? "" : "NOT ") + " a prime number");
+
     }
 
     public static double calculateInterest(double amount, double interestRate) {
         return (amount * (interestRate / 100));
+    }
+    public static boolean isPrime(int wholeNumber) {
+        if (wholeNumber <= 2){
+            return (wholeNumber == 2);
+        }
+        for (int divisor = 2; divisor < wholeNumber; divisor++){
+            if (wholeNumber % divisor == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
