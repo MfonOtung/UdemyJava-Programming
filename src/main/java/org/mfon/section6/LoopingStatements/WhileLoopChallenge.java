fb61a8057f8b6b436cad205415c28b9add812615
MfonOtung <mfonotung@gmail.com>
2024-01-04 12:48:33 +0000

    Solution to Sum of Digit Challange

---
+++ src/main/java/org/mfon/section6/LoopingStatements/WhileLoopChallenge.java
@@ -0,0 +1,35 @@
+package org.mfon.section6.LoopingStatements;
+
+public class WhileLoopChallenge {
+    public static void main(String[] args) {
+        //System.out.println(isEven(43));
+
+        int evenCount = 0;
+        int oddCount = 0;
+        int number = 4;
+        int finishNumber = 20;
+
+        while (number <= finishNumber) {
+            number++;
+            if (!isEven(number)) {
+                oddCount++;
+                continue;
+            }
+            System.out.println("Even number " + number);
+            evenCount++;
+            if (evenCount >=5){
+                break;
+            }
+        }
+        System.out.println("Total odd numbers found = " + oddCount);
+        System.out.println("Total even numbers found = " + evenCount);
+    }
+
+    public static boolean isEven(int number) {
+        if (number % 2 == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+}
