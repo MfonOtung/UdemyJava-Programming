a9944277b83491c7bc5ef11f2216b8c20ed752c3
MfonOtung <mfonotung@gmail.com>
2024-01-04 21:28:26 +0000

    Sum of a Odd Numbers Within A Range of Numbers

---
+++ src/test/java/org/mfon/section6Test/LoopingStatementsTests/SumOddRangeTest.java
@@ -0,0 +1,19 @@
+package org.mfon.section6Test.LoopingStatementsTests;
+
+import org.junit.jupiter.api.Test;
+import org.mfon.section6.LoopingStatements.SumOddRange;
+
+public class SumOddRangeTest {
+    @Test
+    void sumOfRangeOfOddNumberBetweenOneAneFive (){assert SumOddRange.sumOdd(1, 100) == 2500;}
+    @Test
+    void sumOfRangeOfOddNumberBetweenMinusOneAneOneHundred (){assert SumOddRange.sumOdd(-1, 100) == -1;}
+    @Test
+    void sumOfRangeOfOddNumberBetweenOneHundredAneOneHundred (){assert SumOddRange.sumOdd(100, 100) == 0;}
+    @Test
+    void sumOfRangeOfOddNumberBetweenThirteenAneThirteen (){assert SumOddRange.sumOdd(13, 13) == 13;}
+    @Test
+    void sumOfRangeOfOddNumberBetweenMinusOneHundredAneOneHundred (){assert SumOddRange.sumOdd(100, -100) == -1;}
+    @Test
+    void sumOfRangeOfOddNumberBetweenOneHundredAneOneThousand (){assert SumOddRange.sumOdd(100, 1000) == 247500;}
+}
