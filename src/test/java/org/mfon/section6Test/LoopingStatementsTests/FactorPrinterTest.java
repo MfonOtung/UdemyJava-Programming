cbbce822c17dcfd321e5557ee4579414f479c1dc
MfonOtung <mfonotung@gmail.com>
2024-01-06 10:25:12 +0000

    Solution to get Factors of a Number

---
+++ src/test/java/org/mfon/section6Test/LoopingStatementsTests/FactorPrinterTest.java
@@ -0,0 +1,9 @@
+package org.mfon.section6Test.LoopingStatementsTests;
+
+import org.junit.jupiter.api.Test;
+import org.mfon.section6.LoopingStatements.FactorPrinter;
+
+public class FactorPrinterTest {
+    @Test
+    void factorOfSix (){assert FactorPrinter.printFactors(-1) == 6;}
+}
