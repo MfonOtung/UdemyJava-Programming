7e1b4f9bd3be4e98e03fbaa9a558d0933f7e3d61
MfonOtung <mfonotung@gmail.com>
2024-01-06 16:01:10 +0000

    Solution to get a Perfect Number

---
+++ src/test/java/org/mfon/section6Test/LoopingStatementsTests/PerfectNumberTest.java
@@ -0,0 +1,15 @@
+package org.mfon.section6Test.LoopingStatementsTests;
+
+import org.junit.jupiter.api.Test;
+import org.mfon.section6.LoopingStatements.PerfectNumber;
+
+public class PerfectNumberTest {
+    @Test
+    void sixIsAPerfectNumber (){assert PerfectNumber.isPerfectNumber(6) == true;}
+    @Test
+    void twentyEightIsAPerfectNumber (){assert PerfectNumber.isPerfectNumber(28) == true;}
+    @Test
+    void fiveIsAPerfectNumber (){assert PerfectNumber.isPerfectNumber(5) == false;}
+    @Test
+    void negativeOneIsAPerfectNumber (){assert PerfectNumber.isPerfectNumber(-1) == false;}
+}
