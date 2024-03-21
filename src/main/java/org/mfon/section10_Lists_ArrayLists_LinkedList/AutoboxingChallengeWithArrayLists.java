50fdb6a54b72ac3b9029aef129526c4b5a4eac3c
MfonOtung <mfonotung@gmail.com>
2024-03-21 15:27:46 +0000

    Addition of Program to create a new bank, add a new customer and print out the bank information

--- src/main/java/org/mfon/section10_Lists_ArrayLists_LinkedList/AutoboxingChallengeWithArrayLists.java
+++ src/main/java/org/mfon/section10_Lists_ArrayLists_LinkedList/AutoboxingChallengeWithArrayLists.java
@@ -27,33 +27,77 @@ public class AutoboxingChallengeWithArrayLists {
     public static void main(String[] args) {
         Customer bob = new Customer("Bob S", 1000.0);
         System.out.println(bob);
+
+        Bank bank = new Bank("Chase");
+        bank.addNewCustomer("Jane A", 500.0);
+        System.out.println(bank);
+
+        bank.addTransaction("Jane A", -10.25);
+        bank.addTransaction("jane A", -75.01);
+        bank.printStatement("Jane a");
+
+        bank.addNewCustomer("bob s", 25);
+        bank.addTransaction("Bob S", 100);
+        bank.printStatement("Bob S");
     }
 }
 
 class Bank {
     private String name;
     private ArrayList<Customer> customers = new ArrayList<>(5000);
+
     public Bank(String name) {
         this.name = name;
     }
-    private Customer getCustomer(String customerName) {
 
-        for (var customer : customers) {
-            if (customer.name().equalsIgnoreCase(customerName)) {
-                return customer;
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "name='" + name + '\'' +
+                ", customers=" + customers +
+                '}';
+    }
+
+    private Customer getCustomer (String customerName){
+
+            for (var customer : customers) {
+                if (customer.name().equalsIgnoreCase(customerName)) {
+                    return customer;
+                }
+            }
+            System.out.printf("Customer (%s) wasn't found %n", customerName);
+            return null;
+        }
+
+        public void addNewCustomer (String customerName,double initialDeposit){
+
+            if (getCustomer(customerName) == null) {
+                Customer customer = new Customer(customerName, initialDeposit);
+                customers.add(customer);
+                System.out.println("New Customer added: " + customer);
             }
         }
-        System.out.printf("Customer (%s) wasn't found %n", customerName);
 
-        return null;
-    }
+        public void addTransaction (String name,double transactionAmount){
 
-    public void addNewCustomer(String customerName, double initialDeposit) {
+            Customer customer = getCustomer(name);
+            if (customer != null) {
+                customer.transactions().add(transactionAmount);
+            }
+        }public void printStatement(String customerName) {
 
-        if (getCustomer(customerName) == null) {
-            Customer customer = new Customer(customerName, initialDeposit);
-            customers.add(customer);
-            System.out.println("New Customer added: " + customer);
+            Customer customer = getCustomer(customerName);
+            if (customer == null) {
+                return;
+            }
+
+            System.out.println("-".repeat(30));
+            System.out.println("Customer Name: " + customer.name());
+            System.out.println("Transactions:");
+            for (double d : customer.transactions()) {  // unboxing
+                System.out.printf("$%10.2f (%s)%n", d, d < 0 ? "debit" : "credit");
+            }
+
         }
+
     }
-}
