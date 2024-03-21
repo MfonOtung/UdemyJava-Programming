2eb1ffb2c13463d033d72ad394a0fb0bd9550f33
MfonOtung <mfonotung@gmail.com>
2024-03-21 21:57:02 +0000

    Addition of Switch Statement

---
+++ src/main/java/org/mfon/section10_Lists_ArrayLists_LinkedList/Enum/Topping.java
@@ -0,0 +1,18 @@
+package org.mfon.section10_Lists_ArrayLists_LinkedList.Enum;
+
+public enum Topping {
+    MUSTARD,
+    PICKLES,
+    BACON,
+    CHEDDAR,
+    TOMATO;
+
+    public double getPrice() {
+
+        return switch (this) {
+            case BACON -> 1.5;
+            case CHEDDAR -> 1.0;
+            default -> 0.0;
+        };
+    }
+}
