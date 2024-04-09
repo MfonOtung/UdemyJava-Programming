299944d0df1335e86f1c76dc63bec25d3bc22e21
MfonOtung <mfonotung@gmail.com>
2024-04-09 20:10:36 +0000

    Program to state if a flight object is taking off, flying or landing (Extending Interfaces/Coding to Interface)

--- src/main/java/org/mfon/section11_Abstraction_in_Java/Interfaces/Animal.java
+++ src/main/java/org/mfon/section11_Abstraction_in_Java/Interfaces/Animal.java
@@ -1,4 +1,48 @@
 package org.mfon.section11_Abstraction_in_Java.Interfaces;
+enum FlightStages implements Trackable {GROUNDED, LAUNCH, CRUISE, DATA, COLLECTION;
+
+    @Override
+    public void track() {
+      if (this !=GROUNDED){
+          System.out.println("Monitoring " + this);
+      }
+    }
+}
+record DragonFly(String name, String type) implements FlightEnabled{
+    @Override
+    public void takeoff() {
+
+    }
+    @Override
+    public void land() {
+
+    }
+    @Override
+    public void fly() {
+
+    }
+}
+class Satelite implements OrbitEarth{
+    public void achieveOrbit(){
+        System.out.println("Orbit achieved!");
+    }
+    @Override
+    public void takeoff() {
+
+    }
+    @Override
+    public void land() {
+
+    }
+    @Override
+    public void fly() {
+
+    }
+}
+
+interface OrbitEarth extends FlightEnabled{
+    void achieveOrbit();
+}
 interface FlightEnabled{
 //    public abstract void takeoff();
 //    abstract void land();
--- src/main/java/org/mfon/section11_Abstraction_in_Java/Interfaces/Main.java
+++ src/main/java/org/mfon/section11_Abstraction_in_Java/Interfaces/Main.java
@@ -1,5 +1,9 @@
 package org.mfon.section11_Abstraction_in_Java.Interfaces;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -18,10 +22,29 @@ public class Main {
         inFlight(new Jet());
         Trackable truck = new Truck();
         truck.track();
+        //inFlight(flier);
+        inFlight(new Butterfly());
 
         double kmstraveled = 100;
         double milesTraveled = kmstraveled * FlightEnabled.KM_TO_MILES;
         System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmstraveled, milesTraveled);
+
+//        ArrayList<FlightEnabled> fliers = new ArrayList<>();
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        //List<FlightEnabled> butterFliers = new ArrayList<>();
+        List<FlightEnabled> butterFliers = new LinkedList<>(); //This can be refactored to LinkedList and it will still compile
+        butterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(butterFliers);
+        flyFliers(butterFliers);
+        landFliers(butterFliers);
+        //This doesn't compile so we change the arraylist to linkedList
     }
     private static void inFlight(FlightEnabled flier){
         flier.takeoff();
@@ -30,5 +53,23 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+//    private static void triggerFliers(ArrayList<FlightEnabled> fliers){
+    private static void triggerFliers(List<FlightEnabled> fliers){
+        for (var flier : fliers){
+            flier.takeoff();
+        }
+    }
+//    private static void flyFliers(ArrayList<FlightEnabled> fliers){
+    private static void flyFliers(List<FlightEnabled> fliers){
+        for (var flier : fliers){
+            flier.fly();
+        }
+    }
+//    private static void landFliers(ArrayList<FlightEnabled> fliers){
+private static void landFliers(List<FlightEnabled> fliers){
+        for (var flier : fliers){
+            flier.land();
+        }
     }
 }
