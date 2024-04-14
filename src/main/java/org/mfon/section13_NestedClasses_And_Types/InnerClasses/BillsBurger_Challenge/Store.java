024134a046afd7404ea35c67db9419af65852466
MfonOtung <mfonotung@gmail.com>
2024-04-14 22:26:45 +0000

    Creating Codes for Converting Prices of Items, using an Inner Class, to build a composition inside of a class

---
+++ src/main/java/org/mfon/section13_NestedClasses_And_Types/InnerClasses/BillsBurger_Challenge/Store.java
@@ -0,0 +1,14 @@
+package org.mfon.section13_NestedClasses_And_Types.InnerClasses.BillsBurger_Challenge;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        Meal regularMeal = new Meal();
+        System.out.println(regularMeal);
+
+        Meal USRegularMeal = new Meal(0.68);
+        System.out.println(USRegularMeal);
+
+    }
+}
