ee3ba866f329997bdcd7a4707aa1834499e0a85f
MfonOtung <mfonotung@gmail.com>
2024-04-16 14:57:13 +0000

    Creating Codes for Local Class to Add Extra Functionality to an Existing Class

---
+++ src/main/java/org/mfon/section13_NestedClasses_And_Types/Local_Classes/domain/EmployeeComparator.java
@@ -0,0 +1,12 @@
+package org.mfon.section13_NestedClasses_And_Types.Local_Classes.domain;
+
+import java.util.Comparator;
+
+public class EmployeeComparator <T extends Employee> implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.getName().compareTo(o2.getName());
+    }
+}
+
---
+++ src/main/java/org/mfon/section13_NestedClasses_And_Types/Playlist_Coding_Challenge/Song.java
@@ -0,0 +1,23 @@
+package org.mfon.section13_NestedClasses_And_Types.Playlist_Coding_Challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Song {
+   private String title;
+   private double duration;
+
+   public Song(String title, double duration) {
+      this.title = title;
+      this.duration = duration;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   //private List<String> song = new ArrayList<>();
+
+
+
+}
